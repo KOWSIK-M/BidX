03c9ec8b83e5a2139657d2f37180c8c9376a71bd
KOWSIK-M <2200030358@kluniversity.in>
2024-05-01 01:30:18 +0530

    deployed website

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function Website() {
       <div style={{ background: color }}>
         <Routes>
           <Route
-            path="/"
+            path="/BidX"
             element={
               <Home changeColor={changeColor} />
             }
